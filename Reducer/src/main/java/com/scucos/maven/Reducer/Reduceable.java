02b385a3b942842ea22d87a784751e109fe9c9fe
Steffen Cucos <scucos@venacorp.com>
2020-01-27 15:57:26 -0500

    Remove unsued Reduceable interface

--- Reducer/src/main/java/com/scucos/maven/Reducer/Reduceable.java
+++
@@ -1,7 +0,0 @@
-package com.scucos.maven.Reducer;
-
-import com.scucos.maven.Reducer.Slice.SliceConstructionException;
-
-public interface Reduceable<T> {
-	Slice<T> toSlice() throws SliceConstructionException;
-}
