7ccdf9876e67c1c93ae86795162b242fa6a7bf54
Steffen Cucos <scucos@venacorp.com>
2020-01-27 15:31:45 -0500

    Include default map reducer

---
+++ Reducer/src/main/java/com/scucos/maven/Reducer/MapReducer.java
@@ -0,0 +1,19 @@
+package com.scucos.maven.Reducer;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.scucos.maven.Reducer.Slice.ObjectConstructionException;
+import com.scucos.maven.Reducer.Slice.SliceConstructionException;
+
+public class MapReducer<K, V> extends AbstractReducer<Map<K, Collection<V>>> {	
+	@Override
+	public Slice<Map<K, Collection<V>>> toSlice(Map<K, Collection<V>> t) throws SliceConstructionException {
+		return new Slice<>(t);
+	}
+
+	@Override
+	public Map<K, Collection<V>> fromSlice(Slice<Map<K, Collection<V>>> slice) throws ObjectConstructionException {
+		return Slice.toMap(slice);
+	}
+}
