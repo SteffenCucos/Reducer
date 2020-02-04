969a990e82d025f1aa726e275bc85b2f78ea23cf
Steffen Cucos <scucos@venacorp.com>
2020-02-04 15:51:31 -0500

    start on GraphReducer implementation

---
+++ Reducer/src/main/java/com/scucos/maven/Reducer/GraphDistanceReducer.java
@@ -0,0 +1,33 @@
+package com.scucos.maven.Reducer;
+
+import java.util.Set;
+
+public abstract class GraphDistanceReducer<T> implements Reducer<T> {
+
+	@Override
+	public Set<Slice<T>> reduceSlices(Set<Slice<T>> slices) {
+		// TODO Auto-generated method stub
+		
+		//Work on reducing closed cycles in graph of slices where edges are drawn between slices which have distance = 1
+		
+		
+		//the graph can be represented as a matrix like so:
+		
+		/*
+		 * say we have 4 slices like this
+		 * 1 = [1,A] 2 = [1,B] 3 = [2,A] 4 = [2,C]
+		 * 
+		 * which yield this graph, where 1 represents an edge, and x represents no edge
+		 * We only need to fill in the top half (minus the diagonal) because the graph is
+		 * bidirectional and so the other half would just be the mirror of the first half
+		 * 
+		 *   1  2  3  4
+		 * 1 x  1  1  x
+		 * 2 ---x  1  x
+		 * 3 ------x  1
+		 * 4 ---------x
+		 */
+		
+		return null;
+	}	
+}
