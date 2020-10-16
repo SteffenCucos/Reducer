ce1a8540843f376c2e7586fee196e045711777db
Steffen CUcos <steffen.cucos@gmail.com>
2020-10-16 01:30:19 -0400

    Refactor Slice.java, organize methods, add tClass field to simplify the toType method, simplify the getFieldAsType method

--- Reducer/src/main/java/com/scucos/maven/Reducer/Slice.java
+++ Reducer/src/main/java/com/scucos/maven/Reducer/Slice.java
@@ -1,15 +1,13 @@
 package com.scucos.maven.Reducer;
 
+import java.util.stream.Collectors;
 import java.lang.reflect.Field;
 import java.util.Collection;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import com.scucos.maven.Reducer.RecursiveReducer.CollectionNode;
 
 /**
  * Slice<T> objects are used to encapsulate a rectangular slice of the N dimensional space formed from a Set<Slice<T>> objects
@@ -21,7 +19,7 @@ import com.scucos.maven.Reducer.RecursiveReducer.CollectionNode;
  */
 public class Slice<T> implements Comparable<Slice<T>> {
 
-	private Map<Object, Collection<?>> slice = new HashMap<>();
+	private Map<Object, Collection<?>> sliceMap = new HashMap<>();
 	
 	@SuppressWarnings("serial")
 	public static class SliceConstructionException extends RuntimeException {
@@ -37,15 +35,86 @@ public class Slice<T> implements Comparable<Slice<T>> {
 		}
 	}
 	
-	public Slice() {
-
+	// Constructors
+	
+	Class<T> tClass;
+	
+	public Slice(Class<T> tClass) throws SliceConstructionException {
+		this.tClass = tClass;
 	}
 	
-	public Slice(Map<Object, Collection<?>> slice) {
-		this.slice = slice;
+	@SuppressWarnings("unchecked")
+	public Slice(T t, Class<? extends Object> tClass) throws SliceConstructionException {
+		
+		this.tClass = (Class<T>) tClass;
+		
+		//Only work on the top level fields (ignore super fields)
+		for(Field field : t.getClass().getDeclaredFields()) {
+			
+			String name = field.getName();
+			Class<?> fieldClass = field.getType();
+			Class<?> collectionClass = Collection.class;
+			boolean isCollection = collectionClass.isAssignableFrom(fieldClass);
+			if(isCollection) {
+				field.setAccessible(true);
+				
+				try {
+					sliceMap.put(name, (Collection<Object>) field.get(t));
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new SliceConstructionException(e.getMessage());
+				}
+			}
+		}
 	}
 	
-
+	// sliceMap operations
+	
+	public int getWidth() {
+		return this.sliceMap.size();
+	}
+	
+	public Set<Object> getCategories() {
+		return this.sliceMap.keySet();
+	}
+	
+	public void addEntry(Object category, Collection<?> objects) {
+		this.sliceMap.put(category, objects);
+	}
+	
+	public Collection<?> getEntry(Object category) {
+		return this.sliceMap.get(category);
+	}
+	
+	public void deleteEntry(Object category) {
+		this.sliceMap.remove(category);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void addObjects(Object category, Collection<?> objects) {
+		Collection<Object> thisObjects = (Collection<Object>) this.getEntry(category);
+		thisObjects.addAll(objects);
+	}
+	
+	public Map<Object, Collection<?>> getMap() {
+		return sliceMap;
+	}
+	
+	public int volume() {
+		return this.getMap().values().stream().map(c -> c.size()).reduce(1, (accumulator, current) -> accumulator*current);
+	}
+	
+	public String toString() {
+		return sliceMap.toString();
+	}
+	
+	// Slice operations
+	
+	@Override
+	public int compareTo(Slice<T> other) {
+		int otherVolume = other.volume();
+		int volume = volume();
+		return (volume > otherVolume) ? -1 : (volume == otherVolume) ? 0 : 1;
+	}
 	
 	public boolean containedIn(Slice<T> into) {
 		for(Object category : getCategories()) {
@@ -59,8 +128,8 @@ public class Slice<T> implements Comparable<Slice<T>> {
 		return true;
 	}
 	
-	public Set<Object> asymetricDifference(Slice<T> into) {
-		Set<Object> diffCategories = new HashSet<>();
+	public List<Object> asymetricDifference(Slice<T> into) {
+		List<Object> diffCategories = new ArrayList<>();
 		for(Object category : getCategories()) {
 			Collection<?> otherObjects = into.getEntry(category);
 			Collection<?> thisObjects = this.getEntry(category);
@@ -77,41 +146,43 @@ public class Slice<T> implements Comparable<Slice<T>> {
 		addObjects(category, otherObjects);
 	}
 	
+	// Reflection utility methods
+	
 	@SuppressWarnings("unchecked")
-	public Slice(T t) throws SliceConstructionException {
-		//Only work on the top level fields (ignore super fields)
-		for(Field field : t.getClass().getDeclaredFields()) {
-			
-			String name = field.getName();
-			Class<?> fieldClass = field.getType();
-			Class<?> collectionClass = Collection.class;
-			boolean isCollection = collectionClass.isAssignableFrom(fieldClass);
-			if(isCollection) {
-				field.setAccessible(true);
-				
-				try {
-					slice.put(name, (Collection<Object>) field.get(t));
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					throw new SliceConstructionException(e.getMessage());
-				}
-			}
-		}
+	public static <T, M, C extends Collection<M>> C getFieldAsType(String category, Slice<T> slice) {
+		return (C) slice.getEntry(category);
 	}
 	
-	public T toType(Class<T> cls) throws ObjectConstructionException {
-		T t = null;
-		try {
-			t = (T) cls.newInstance();
-		} catch (InstantiationException e) {
-			throw new ObjectConstructionException("Class '" + cls.toString() + "' does not have empty constructor");
-		} catch (IllegalAccessException e) {
-			throw new ObjectConstructionException("Unable to create object from slice");
+	@SuppressWarnings("unchecked")
+	public static <K, V> Map<K, Collection<V>> toMap(Slice<Map<K, Collection<V>>> slice) {
+		Map<K, Collection<V>> map = new HashMap<>();
+		
+		for(Object key : slice.sliceMap.keySet()) {
+			map.put((K)key, (Collection<V>) slice.sliceMap.get(key));
 		}
 		
-		for(Object field : slice.keySet()) {
-			Collection<?> objects = slice.get(field);
+		return map;
+	}
+	
+	public T toType() throws ObjectConstructionException {
+		
+		if (tClass == null) {
+			throw new ObjectConstructionException("Slice instance is missing T class object\n");
+		}
+		
+		T t = null;
+		try {
+			t = (T) tClass.newInstance();
+		} catch (InstantiationException e) {
+			throw new ObjectConstructionException("Class '" + tClass.toString() + "' does not have empty constructor\n" + e.toString());
+		} catch (IllegalAccessException e) {
+			throw new ObjectConstructionException("Unable to create object of type '" + tClass.getCanonicalName() + "'\n" + e.toString());
+		}
+		
+		for (Object field : sliceMap.keySet()) {
+			Collection<?> objects = sliceMap.get(field);
 			String fieldName = (String)field;
-			if(!setField(t, fieldName, objects)) {
+			if (!setField(t, fieldName, objects)) {
 				throw new ObjectConstructionException("Unable to set field '" + fieldName + "' while constructing object");
 			}
 		}
@@ -145,74 +216,5 @@ public class Slice<T> implements Comparable<Slice<T>> {
 	    } catch (IllegalAccessException e) {
 	        return false;
 	    }
-	}
-	
-	public int getWidth() {
-		return this.slice.size();
-	}
-	
-	public Set<Object> getCategories() {
-		return this.slice.keySet();
-	}
-	
-	public void addEntry(Object category, Collection<?> objects) {
-		this.slice.put(category, objects);
-	}
-	
-	public Collection<?> getEntry(Object category) {
-		return this.slice.get(category);
-	}
-	
-	public void deleteEntry(Object category) {
-		this.slice.remove(category);
-	}
-
-	@SuppressWarnings("unchecked")
-	public void addObjects(Object category, Collection<?> objects) {
-		Collection<Object> thisObjects = (Collection<Object>) this.getEntry(category);
-		thisObjects.addAll(objects);
-	}
-	
-	public Map<Object, Collection<?>> getMap() {
-		return slice;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static <T, M, C extends Collection> C getFieldAsType(String category, Slice<T> slice, C collection) {
-		List<M> list = (List<M>) slice.getEntry(category)
-				.stream()
-				.map(o -> (M)o)
-				.collect(Collectors.<M>toList());
-		
-		collection.addAll(list);
-		
-		return collection;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static <K, V> Map<K, Collection<V>> toMap(Slice<Map<K, Collection<V>>> slice) {
-		Map<K, Collection<V>> map = new HashMap<>();
-		Map<Object, Collection<?>> sliceMap = slice.getMap();
-		
-		for(Object key : sliceMap.keySet()) {
-			map.put((K)key, (Collection<V>) sliceMap.get(key));
-		}
-		
-		return map;
-	}
-	
-	public String toString() {
-		return slice.toString();
-	}
-	
-	public int volume() {
-		return this.getMap().values().stream().map(c -> c.size()).reduce(1, (accumulator, current) -> accumulator*current);
-	}
-	
-	@Override
-	public int compareTo(Slice<T> other) {
-		int otherVolume = other.volume();
-		int volume = volume();
-		return (volume > otherVolume) ? -1 : (volume == otherVolume) ? 0 : 1;
 	}
 }
